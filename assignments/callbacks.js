249c5c8aaa5987c52cb2d7d9d0a63a797887a009
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-06-11 16:41:47 -0500

    finished challenges for array, callback, closure

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -24,27 +24,57 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log("Length: " + length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(lastItem) {
+  console.log("Last item: " + lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(1, 2, function(sum) {
+  console.log("Sum: " + sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(2, 3, function(product) {
+  console.log("Product: " + product);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let inList = false;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      inList = true;
+    }
+  }
+  return cb(inList);
 }
+
+contains("yo-yo", items, function(bool) {
+  console.log(bool);
+});
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,17 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function whichMonth() {
+  const month = "June";
+  function displayMonth() {
+    console.log("The current month is " + month);
+  }
+  displayMonth();
+}
+
+whichMonth();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
